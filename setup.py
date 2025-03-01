a8f264d4faf2f8e798dea23a3cdaef6c9ac5e986
Andy Paul Chen <la.vache.qui.vit@gmail.com>
2025-03-01 11:03:42 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="poshcar",
     version="2.0.2",
-    description="Crystal structure engine with VASP POSCAR formats"
+    description="Crystal structure engine with VASP POSCAR formats",
     packages=find_packages(),
     install_requires=[
         "ase",
